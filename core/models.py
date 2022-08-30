01a12840e1d3f01f334bd072624cd936461f1136
raiany <raianyftb@gmail.com>
2022-08-30 14:26:33 -0300

    Atualizando cruds

--- core/models.py
+++ core/models.py
@@ -1,7 +1,11 @@
+from distutils.archive_util import make_zipfile
 from django.db import models
 
-class Cafe(models.Model): 
-    marca = models.CharField('Marca', max_length=100)
+class Produtos(models.Model): 
+    nome = models.CharField('Nome', max_length=100)
     tipo = models.CharField('Tipo', max_length=100)
+    marca = models.CharField('Marca', max_length=100)
     quantidade = models.IntegerField('Quantidade')
+    descricao = models.CharField('Descrição', max_length=200)
+     
 
